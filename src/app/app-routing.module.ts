f759fbf14d0bf309ffb27b7f35ee07b4addf72a4
gvrito <gvritishvili.gio123@gmail.com>
2021-06-22 21:01:24 +0400

    init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { TestComponent } from './Features/test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'ytmp3',
+    pathMatch: 'full'
+  },
+  {
+    path: 'test',
+    component: TestComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './Features/navigation/navigation.component';
+import { HeaderComponent } from './Features/header/header.component';
+import { TestComponent } from './Features/test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HeaderComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
